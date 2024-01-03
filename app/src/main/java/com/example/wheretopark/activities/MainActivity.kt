9f36a4fd7383f80297b5c5d528eb6be974501035
Tarik Čačković <113112797+tarikcacak@users.noreply.github.com>
2024-01-03 17:23:48 +0100

    Built bottom navigation for main activity

--- app/src/main/java/com/example/wheretopark/activities/MainActivity.kt
+++ app/src/main/java/com/example/wheretopark/activities/MainActivity.kt
@@ -2,11 +2,23 @@ package com.example.wheretopark.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
 import com.example.wheretopark.R
+import com.example.wheretopark.databinding.ActivityMainBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navController = findNavController(R.id.fragmentMain)
+        binding.btmNavView.setupWithNavController(navController)
     }
 }
