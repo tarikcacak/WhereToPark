53ea6708360d9fd6af494522ed3fb7b2a7e6ae92
Tarik Čačković <113112797+tarikcacak@users.noreply.github.com>
2024-01-25 18:19:12 +0100

    Finished profile fragment viewModel

--- app/src/main/java/com/example/wheretopark/ui/profile/ProfileFragment.kt
+++ app/src/main/java/com/example/wheretopark/ui/profile/ProfileFragment.kt
@@ -5,9 +5,18 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.activityViewModels
 import com.example.wheretopark.R
+import com.example.wheretopark.databinding.FragmentProfileBinding
+import dagger.hilt.android.AndroidEntryPoint
+import dagger.hilt.android.lifecycle.HiltViewModel
 
+@AndroidEntryPoint
 class ProfileFragment : Fragment() {
+
+    private var _binding: FragmentProfileBinding? = null
+    private val binding get() = _binding!!
+    private val viewModel: ProfileViewModel by activityViewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,8 +26,8 @@ class ProfileFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false)
+        _binding = FragmentProfileBinding.inflate(layoutInflater, container, false)
+        return binding.root
     }
 
 }
---
+++ app/src/main/java/com/example/wheretopark/ui/profile/ProfileViewModel.kt
@@ -0,0 +1,134 @@
+package com.example.wheretopark.ui.profile
+
+import android.net.Uri
+import android.util.Log
+import androidx.lifecycle.ViewModel
+import com.example.airmovies.util.Resource
+import com.example.wheretopark.models.ticket.Ticket
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.storage.FirebaseStorage
+import dagger.hilt.android.lifecycle.HiltViewModel
+import io.reactivex.rxjava3.subjects.BehaviorSubject
+import io.reactivex.rxjava3.subjects.PublishSubject
+import javax.inject.Inject
+
+@HiltViewModel
+class ProfileViewModel @Inject constructor(
+    private val firebaseAuth: FirebaseAuth,
+    private val firebaseFirestore: FirebaseFirestore,
+    private val firebaseStorage: FirebaseStorage
+) : ViewModel() {
+    private val creditSubject: PublishSubject<String> = PublishSubject.create()
+    private val userSubject: PublishSubject<String> = PublishSubject.create()
+    private val pictureSubject: PublishSubject<String> = PublishSubject.create()
+    private val emailSubject: PublishSubject<String> = PublishSubject.create()
+    private val platesSubject: PublishSubject<String> = PublishSubject.create()
+    private val ticketsSubject: PublishSubject<List<Ticket>> = PublishSubject.create()
+    private val ticketSubject: BehaviorSubject<Resource<Ticket>> = BehaviorSubject.createDefault(Resource.Unspecified())
+
+    val currentUid = firebaseAuth.currentUser?.uid.toString()
+
+    fun getUserData() {
+        firebaseFirestore.collection("user").whereEqualTo("uid", currentUid)
+            .addSnapshotListener { snapshot, exception ->
+                if (exception != null) {
+                    Log.e("MapViewModel", "Error fetching user data: ${exception.message}")
+                    return@addSnapshotListener
+                } else {
+                    if (!snapshot!!.isEmpty) {
+                        val documentList = snapshot.documents
+
+                        for (document in documentList) {
+                            val credits = document.get("credits") as String
+                            val user = document.get("username") as String
+                            val picture = document.get("imgPath") as String
+                            val email = document.get("email") as String
+                            val plates = document.get("plates") as String
+                            creditSubject.onNext(credits)
+                            userSubject.onNext(user)
+                            pictureSubject.onNext(picture)
+                            emailSubject.onNext(email)
+                            platesSubject.onNext(plates)
+                        }
+                    }
+                }
+            }
+    }
+
+    fun getTicketData() {
+        firebaseFirestore.collection("ticket").whereEqualTo("uid", currentUid)
+            .addSnapshotListener { snapshot, exception ->
+                if (exception != null) {
+                    Log.e("MapViewModel", "Error fetching ticket data: ${exception.message}")
+                    return@addSnapshotListener
+                } else {
+                    if (!snapshot!!.isEmpty) {
+                        val ticketList = mutableListOf<Ticket>()
+
+                        for (document in snapshot.documents) {
+                            val location = document.getString("location") ?: ""
+                            val expiring = document.getString("expiring") ?: ""
+                            val uid = document.getString("uid") ?: ""
+
+                            val ticket = Ticket(location, expiring, uid)
+                            ticketList.add(ticket)
+                        }
+                        ticketsSubject.onNext(ticketList)
+                    }
+                }
+            }
+    }
+
+    fun deleteTicket(ticket: Ticket) {
+        val location = ticket.location
+
+        firebaseFirestore.collection("ticket")
+            .whereEqualTo("uid", currentUid)
+            .whereEqualTo("location", location)
+            .addSnapshotListener { snapshot, exception ->
+                if (exception != null) {
+                    Log.e("MapViewModel", "Error deleting ticket: ${exception.message}")
+                    return@addSnapshotListener
+                } else {
+                    if (!snapshot!!.isEmpty) {
+                        for (document in snapshot.documents) {
+                            val uid = document.getString("uid")
+                            val loc = document.getString("location")
+                            if (uid == currentUid && loc == location) {
+                                document.reference.delete()
+                                    .addOnSuccessListener {
+                                        ticketSubject.onNext(Resource.Success(ticket.copy()))
+                                    }
+                                    .addOnFailureListener { e ->
+                                        Log.e("MapViewModel", "Error deleting ticket: ${e.message}")
+                                        ticketSubject.onNext(Resource.Error("Error deleting ticket"))
+                                    }
+                            }
+                        }
+                    }
+                }
+            }
+    }
+
+    fun saveImage(pickedImage: Uri) {
+        firebaseStorage.reference.child("imgPath").child(currentUid!!).delete()
+        val storage = firebaseStorage.reference.child("imgPath").child(currentUid)
+        storage.putFile(pickedImage)
+        storage.downloadUrl.addOnSuccessListener { uri ->
+            val imageUrl = uri.toString()
+
+            firebaseFirestore.collection("user").document(currentUid)
+                .update(
+                    mapOf(
+                        "imgPath" to imageUrl
+                    )
+                )
+        }
+    }
+
+    fun logOut(){
+        firebaseAuth.signOut()
+    }
+
+}
